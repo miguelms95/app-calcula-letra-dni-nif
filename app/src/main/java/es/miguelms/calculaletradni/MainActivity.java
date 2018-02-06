d750e45841287167571659dc7b8b4a6e681c7476
Miguel Martínez Serrano <miguelms.ovi@gmail.com>
2018-02-06 18:29:00 +0100

    button copy to clipboard and implementation

--- app/src/main/java/es/miguelms/calculaletradni/MainActivity.java
+++ app/src/main/java/es/miguelms/calculaletradni/MainActivity.java
@@ -1,5 +1,8 @@
 package es.miguelms.calculaletradni;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,7 +12,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static java.lang.String.format;
 import static java.lang.String.valueOf;
 
 /**
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txLetraDni;
     private EditText txNumerosDni;
+    private View btCopiar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btCalcular = findViewById(R.id.btCalcular);
+        btCopiar = findViewById(R.id.btCopiar);
+        
         txLetraDni = findViewById(R.id.txLetraDni);
         txNumerosDni = findViewById(R.id.txNumerosDNI);
 
@@ -37,17 +42,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btCopiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                copiarAlPortapapeles();
+            }
+        });
+        
+        
+    }
+
+    private void copiarAlPortapapeles() {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("texto", txLetraDni.getText().toString());
+        if (clipboard != null) {
+            clipboard.setPrimaryClip(clip);
+        }
     }
 
     private void calcularLetraDNI() {
         if(txNumerosDni.getText().toString().length() == 8){
+            String caracteresDivision = "TRWAGMYFPDXBNJZSQVHLCKE";
+
             int valor = Integer.valueOf(txNumerosDni.getText().toString());
             int resultado = valor%23;
-            String caracteresDivision = "TRWAGMYFPDXBNJZSQVHLCKE";
+
             String letraDNI = valueOf(caracteresDivision.charAt(resultado));
             Toast.makeText(this, "La letra es " + letraDNI, Toast.LENGTH_SHORT).show();
-            txLetraDni.setText(format("%s %s", txNumerosDni.getText().toString(), letraDNI));
+            txLetraDni.setText(String.format("%s %s", txNumerosDni.getText().toString(), letraDNI));
             ocultaTeclado();
+            btCopiar.setVisibility(View.VISIBLE);
         }else{
             Toast.makeText(getApplicationContext(),
                     R.string.error_longitud_caracteres,Toast.LENGTH_LONG).show();
@@ -58,7 +82,8 @@ public class MainActivity extends AppCompatActivity {
     private void ocultaTeclado(){
         View view = this.getCurrentFocus();
         if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            InputMethodManager imm;
+            imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
             if (imm != null)
                 imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
